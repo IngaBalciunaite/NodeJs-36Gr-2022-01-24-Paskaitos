1ca84af46143cfa03eb6f2de0be2cae0534f0848
Vilius Ramulionis <vilius.ramulionis@gmail.com>
2022-05-25 18:10:33 +0300

    Recipes task

---
+++ 2022-05-24/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+
+const __dirname = dirname( fileURLToPath(import.meta.url) )
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/templates/index.html')
+})
+
+app.listen(3000)
